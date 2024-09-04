5b49ffad631eee745750e1e3e673e925f0ba4b6d
Fernandasacosta <fernandasacostaa@gmail.com>
2024-09-04 17:34:15 -0300

    feat: add new Marquee component

---
+++ components/marquee.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from 'next/image';
+import React from "react";
+import { SiNodedotjs, SiReact, SiJavascript, SiNextdotjs, SiCsharp, SiAngular, SiHtml5, SiCss3, SiPostgresql, SiDbeaver, SiMicrosoftsqlserver, SiMongodb, SiPrisma, SiTailwindcss, SiMysql, SiTypescript } from "react-icons/si";
+import { FaSpaceShuttle } from 'react-icons/fa'; // Importar o ícone de espaço vazio
+
+const StarIcon = () => (
+  <Image
+    src="/star.png"
+    alt="Star"
+    width={24}
+    height={24}
+    className="mx-8"
+  />
+);
+
+const techIcons = [
+  SiNodedotjs,
+  SiReact,
+  SiJavascript,
+  SiNextdotjs,
+  SiCsharp,
+  SiAngular,
+  SiHtml5,
+  SiCss3,
+  SiPostgresql,
+  SiDbeaver,
+  SiMicrosoftsqlserver,
+  SiMongodb,
+  SiPrisma,
+  SiTailwindcss,
+  SiMysql,
+  SiTypescript,
+  
+];
+
+const Marquee = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-4 lg:py-6 overflow-hidden flex justify-center">
+      <div className="max-w-6xl w-full mx-auto">
+        <div className="relative flex items-center whitespace-nowrap overflow-hidden">
+          {/* Cria um wrapper que garante que o conteúdo está visível */}
+          <motion.div
+            className="flex items-center space-x-16"
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ repeat: Infinity, ease: "linear", duration: 10 }}
+          >
+            {techIcons.map((Icon, index) => (
+              <React.Fragment key={index}>
+                <Icon className="text-white text-6xl" />
+                {index < techIcons.length - 1 && (
+                  <StarIcon />
+                )}
+              </React.Fragment>
+            ))}
+            <StarIcon />
+          </motion.div>
+          {/* Duplicar o conteúdo para garantir a rolagem contínua */}
+          <motion.div
+            className="absolute top-0 flex items-center space-x-16"
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            transition={{ repeat: Infinity, ease: "linear", duration: 10  }}
+          
+          >
+            {techIcons.map((Icon, index) => (
+              <React.Fragment key={index}>
+                <Icon className="text-white text-6xl" />
+                {index < techIcons.length - 1 && (
+                  <StarIcon />
+                )}
+              </React.Fragment>
+            ))}
+            <StarIcon />
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Marquee;
